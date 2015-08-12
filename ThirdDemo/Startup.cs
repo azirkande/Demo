f8d54d368135ff9779930f373a9ff2c86e03d9f8
azirkande <amrita.dhange@gmail.com>
2015-08-12 16:46:26 +1000

    initial commit

---
+++ ThirdDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThirdDemo.Startup))]
+namespace ThirdDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
